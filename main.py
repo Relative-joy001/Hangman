eb6a88f8d2c6c930b083f3c52c36b5ad9108b55c
Relative-joy001 <oguntim@gmail.com>
2024-12-22 22:48:17 -0800

    hangman

---
+++ main.py
@@ -0,0 +1,95 @@
+# Hangman game code
+from wordslist import words
+import random
+
+# Load the word list
+
+# def load_word_list():
+#     with open('words.txt', 'r') as file:
+#         words = file.read().splitlines()
+#     return words
+
+# dictionary of key:()
+
+hangman_art = {0: ("    ",
+                   "    ",
+                   "    "),
+               1: ("  o ",
+                   "    ",
+                   "    "),
+               2: ("  o ",
+                   "  | ",
+                   "    "),
+               3: ("  o ",
+                   " /| ",
+                   "    "),
+               4: ("  o ",
+                   " /|\\",
+                   "    "),
+               5: ("  o ",
+                   " /|\\",
+                   " /  "),
+               6: ("  o ",
+                   " /|\\",
+                   " / \\ ")}
+
+def display_man(wrong_guesses):
+    print("**********************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("**********************")
+
+def display_hint(hint):
+    print("Hint:", " ".join(hint))
+
+def display_answer(answer):
+    print("Answer:", " ".join(answer))
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Guess a letter: ").lower()
+        
+        #display_answer(answer)
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input")
+            continue
+
+        if guess in guessed_letters:
+            print(f"You've already guessed that letter: {guess}")
+            continue
+
+        guessed_letters.add(guess)
+
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("You Won!")
+            is_running = False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("You Lost!")
+            is_running = False
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ wordslist.py
@@ -0,0 +1,1 @@
+words = ("apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon")
